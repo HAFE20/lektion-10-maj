5155a471d1162d9eba734dbc0c7201b6cf706d47
Christoffer Wallenberg <christoffer.wallenberg@zocom.se>
2021-05-10 13:59:40 +0200

    Full example

--- dice-game-example/dist/index.js
+++ dice-game-example/dist/index.js
@@ -1,3 +1,12 @@
+/**
+ * Typescript - 3 centrala koncept
+ *
+ * 1. Alla variabel deklarationer ska ha en datatyp (inklusive funktionsparametrar etc)
+ * 2. Definera interface (en sorts ritning) för hur ett objekt ska se ut
+ * 3. Ska en funktion returna något och isåfall vad för datatyp? Ifall den inte
+ * returnerar något så blir det void
+ *
+ */
 let throws = 0; //Antal kast som har gjorts
 let currentGoal = 1; //Nuvarande tärningsmål som ska slås
 let lastThrow = 1; //Tidigare tärningskast
@@ -9,8 +18,35 @@ let dice = {
 };
 const buttonElem = document.querySelector('button');
 const diceElem = document.querySelector('#diceElem');
+function resetUI(numberOfDices) {
+    for (let i = 1; i <= numberOfDices; i++) {
+        document.querySelector(`.dots-${i}`).classList.add('faded'); //Loopa igenom varje tärning och lägg på klassen faded
+    }
+}
 buttonElem.addEventListener('click', () => {
+    //Kastar tärning och sparar resultatet
     let result = dice.throw();
+    //Visar tärningen i gränssnittet
+    diceElem.classList.remove(`dots-${lastThrow}`);
     diceElem.classList.add('dice', `dots-${result}`);
     console.log(`Du slog en ${result}`);
+    throws++; //Öka antal kast med 1
+    //Om resultat är lika med currentGoal och currentGoal är mindre än 6
+    if (result === currentGoal && currentGoal < 6) {
+        console.log('Yay, ökar målet');
+        document.querySelector(`.dots-${result}`).classList.remove('faded');
+        currentGoal++;
+    }
+    //Annars om resultat är lika med currentGoal och currentGoal är lika med 6
+    else if (result === currentGoal && currentGoal === 6) {
+        console.log(`Du kastade totalt ${throws} kast!`);
+        document.querySelector(`.dots-${result}`).classList.remove('faded');
+        setTimeout(() => {
+            alert(`Du kastade totalt ${throws} kast!`);
+            throws = 0;
+            currentGoal = 1;
+            resetUI(6);
+        }, 100);
+    }
+    lastThrow = result;
 });
--- dice-game-example/src/index.ts
+++ dice-game-example/src/index.ts
@@ -1,3 +1,14 @@
+/**
+ * Typescript - 3 centrala koncept
+ * 
+ * 1. Alla variabel deklarationer ska ha en datatyp (inklusive funktionsparametrar etc)
+ * 2. Definera interface (en sorts ritning) för hur ett objekt ska se ut
+ * 3. Ska en funktion returna något och isåfall vad för datatyp? Ifall den inte
+ * returnerar något så blir det void
+ * 
+ */
+
+
 let throws: number = 0; //Antal kast som har gjorts
 let currentGoal: number = 1; //Nuvarande tärningsmål som ska slås
 let lastThrow: number = 1; //Tidigare tärningskast
@@ -17,9 +28,44 @@ let dice: Dice = {
 const buttonElem: HTMLElement = document.querySelector('button');
 const diceElem: HTMLElement = document.querySelector('#diceElem');
 
+function resetUI(numberOfDices: number): void {
+  for(let i: number = 1; i <= numberOfDices; i++) {
+    document.querySelector(`.dots-${i}`).classList.add('faded'); //Loopa igenom varje tärning och lägg på klassen faded
+  }
+}
+
 buttonElem.addEventListener('click', () => {
+  //Kastar tärning och sparar resultatet
   let result: number = dice.throw();
 
+  //Visar tärningen i gränssnittet
+  diceElem.classList.remove(`dots-${lastThrow}`);
   diceElem.classList.add('dice', `dots-${result}`);
   console.log(`Du slog en ${result}`);
+
+  throws++ //Öka antal kast med 1
+
+  //Om resultat är lika med currentGoal och currentGoal är mindre än 6
+  if (result === currentGoal && currentGoal < 6) {
+    console.log('Yay, ökar målet');
+
+    document.querySelector(`.dots-${result}`).classList.remove('faded');
+
+    currentGoal++;
+  }
+  //Annars om resultat är lika med currentGoal och currentGoal är lika med 6
+  else if (result === currentGoal && currentGoal === 6) {
+    console.log(`Du kastade totalt ${throws} kast!`);
+
+    document.querySelector(`.dots-${result}`).classList.remove('faded');
+
+    setTimeout(() => {
+      alert(`Du kastade totalt ${throws} kast!`);
+      throws = 0;
+      currentGoal = 1;
+
+      resetUI(6);
+    }, 100);
+  }
+  lastThrow = result;
 });
